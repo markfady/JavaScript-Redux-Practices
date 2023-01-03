3fe50c8fefeaeaedf957866dc2e97287724131d4
Markfady <47190433+markfady@users.noreply.github.com>
2023-01-03 20:10:23 +0200

    Add files via upload

--- Redux JavaScript/script.js
+++ Redux JavaScript/script.js
@@ -2,7 +2,7 @@
     const WITHDRAW_MONEY="WITHDRAW_MONEY";
     const DEPOSITE_MONEY="DEPOSITE_MONEY";
     const ADD_PRODUCT="ADD_PRODUCT";
-
+    const GET_PRODUCTS="GET_PRODUCT";
 //Action Creators
 // const action={
 //     type:"WITHDRAW_MONEY"      
@@ -26,6 +26,20 @@ const addProduct=(product)=>{
         payload:product
     }
 }
+//Getting data from API using redux , here we must use redux thunk(middleware) because it is asynchronous function (Actions default is synchronous)
+const getProduct=(products)=>{   
+    return {
+        type:GET_PRODUCTS,
+        payload:products
+    }
+}
+const fetchProducts=()=>{           //Using Redux thunk now actions can return functions not only plain objects
+        return async(dispatch)=>{       //The function has access to dispatch function to trigger specific action 
+            const data=await fetch('https://fakestoreapi.com/products/1');
+            const result=await data.json();
+            dispatch(getProduct(result))        //Triggering getProduct action carrying with it the data returned from the fetchAPI
+        }
+}
 //Reducers
 const bankReducer=(state=1000,action)=>{
     switch(action.type){
@@ -41,16 +55,19 @@ const productReducer=(state=[],action)=>{
     switch(action.type){
         case ADD_PRODUCT:
             return [...state ,action.payload];   //Never mutate the state direct : don't push direct to state make a new state first then push the payload you added 
+            case GET_PRODUCTS:
+                return [action.payload] ;   //Return the data came from payload (result) > payload:products
             default:
                 return state;
     }
 }
-//Store Creation From Redux
+//we use combieReducers if we have more than one reducer in the application
 const appReducer=Redux.combineReducers({
     bank:bankReducer,
     products:productReducer
 })
-const store=Redux.createStore(appReducer);
+//Store Creation From Redux , plus we put in the enhancer the MiddleWare we want to use to fetch data using asynchronous Functions
+const store=Redux.createStore(appReducer,Redux.applyMiddleware(ReduxThunk));
 
 store.subscribe(()=>{           //after each dispatch subscribe will return the new state but , get state only render one time 
     console.log("Current State", store.getState());
